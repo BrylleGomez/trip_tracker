9b22e5a8193e95dde165f43c94c907d9711b36a8
BrylleGomez <brylle.gomez@gmail.com>
2021-12-30 22:55:40 +0400

    Added prelim add route functionality

--- lib/models/route.dart
+++ lib/models/route.dart
@@ -1,7 +1,7 @@
-class Route {
+class TripRoute {
   final String id;
   final String name;
   final String path;
 
-  const Route({required this.id, required this.name, required this.path});
+  const TripRoute({required this.id, required this.name, required this.path});
 }
--- lib/pages/routes_page.dart
+++ lib/pages/routes_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:trip_tracker/models/route.dart';
+import 'package:trip_tracker/utils/get_random_id.dart';
 
 class RoutesPage extends StatefulWidget {
   const RoutesPage({Key? key}) : super(key: key);
@@ -8,15 +10,33 @@ class RoutesPage extends StatefulWidget {
 }
 
 class _RoutesPageState extends State<RoutesPage> {
+  final List<TripRoute> routes = [];
+
+  void addNewRoute() {
+    final newRoute = TripRoute(id: getRandom(6), name: 'Name', path: 'Path');
+    setState(() {
+      routes.add(newRoute);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text(
-            'Routes...',
-            style: Theme.of(context).textTheme.headline4,
+          TextButton.icon(
+              onPressed: addNewRoute,
+              icon: const Icon(Icons.add_location_alt_rounded),
+              label: const Text('Add New Route')),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: routes.length,
+            itemBuilder: (BuildContext context, int index) {
+              // access element from list using index
+              // you can create and return a widget of your choice
+              return Text(routes[index].name);
+            },
           ),
         ],
       ),
