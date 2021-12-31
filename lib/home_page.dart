c1245e36b1b9a3c930cc3a208eb536253eb12c6c
BrylleGomez <brylle.gomez@gmail.com>
2021-12-31 12:14:03 +0400

    Created floating action button to add items

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
 import 'package:trip_tracker/pages/routes_page.dart';
 import 'package:trip_tracker/pages/trips_page.dart';
+
+import 'models/route.dart';
+import 'utils/consts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,30 +15,70 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _currentPageIndex = 0;
+  int _pageIdx = 0;
 
-  final pages = [const RoutesPage(), const TripsPage()];
+  final List<Page> _pages = [
+    Page(
+      navbarIcon: Icons.location_on,
+      label: 'Routes',
+      widget: const RoutesPage(),
+      actionIcon: Icons.add_location_alt_rounded,
+      actionOnPressed: () {
+        const newRoute = TripRoute(name: 'Name', path: 'Path');
+        Hive.box<TripRoute>(hiveRoutesBox).add(newRoute);
+      },
+    ),
+    Page(
+      navbarIcon: Icons.list,
+      label: 'Trips',
+      widget: const TripsPage(),
+      actionIcon: Icons.add_circle,
+      actionOnPressed: () {
+        const newRoute = TripRoute(name: 'Name', path: 'Path');
+        Hive.box<TripRoute>(hiveRoutesBox).add(newRoute);
+      },
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Trip Tracker'),
+        title: Text(_pages[_pageIdx].label),
       ),
-      body: pages[_currentPageIndex],
+      body: _pages[_pageIdx].widget,
+      floatingActionButton: FloatingActionButton(
+        onPressed: _pages[_pageIdx].actionOnPressed,
+        child: Icon(_pages[_pageIdx].actionIcon),
+      ),
       bottomNavigationBar: BottomNavigationBar(
-        items: const [
+        items: [
           BottomNavigationBarItem(
-              icon: Icon(Icons.location_on), label: 'Routes'),
-          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Trips')
+              icon: Icon(_pages[0].navbarIcon), label: _pages[0].label),
+          BottomNavigationBarItem(
+              icon: Icon(_pages[1].navbarIcon), label: _pages[1].label)
         ],
         onTap: (idx) => {
           setState(() {
-            _currentPageIndex = idx;
+            _pageIdx = idx;
           })
         },
-        currentIndex: _currentPageIndex,
+        currentIndex: _pageIdx,
       ),
     );
   }
+}
+
+class Page {
+  final IconData navbarIcon;
+  final String label;
+  final Widget widget;
+  final IconData actionIcon;
+  final Function() actionOnPressed;
+  const Page(
+      {required this.navbarIcon,
+      required this.label,
+      required this.widget,
+      required this.actionIcon,
+      required this.actionOnPressed});
 }
--- lib/pages/routes_page.dart
+++ lib/pages/routes_page.dart
@@ -11,51 +11,23 @@ class RoutesPage extends StatefulWidget {
 }
 
 class _RoutesPageState extends State<RoutesPage> {
-  final List<TripRoute> routes = [];
-
-  void addNewRoute() {
-    const newRoute = TripRoute(name: 'Name', path: 'Path');
-    setState(() {
-      routes.add(newRoute);
-    });
-    Hive.box<TripRoute>(hiveRoutesBox).add(newRoute);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          TextButton.icon(
-              onPressed: addNewRoute,
-              icon: const Icon(Icons.add_location_alt_rounded),
-              label: const Text('Add New Route')),
-          ValueListenableBuilder<Box>(
-            valueListenable: Hive.box<TripRoute>(hiveRoutesBox).listenable(),
-            builder: (context, box, widget) {
-              List<int> keys = box.keys.cast<int>().toList();
-              return ListView.builder(
-                shrinkWrap: true,
-                itemCount: keys.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final int key = keys[index];
-                  final dynamic route = box.get(key);
-                  return Text(route.name);
-                },
-              );
+      child: ValueListenableBuilder<Box>(
+        valueListenable: Hive.box<TripRoute>(hiveRoutesBox).listenable(),
+        builder: (context, box, widget) {
+          List<int> keys = box.keys.cast<int>().toList();
+          return ListView.builder(
+            shrinkWrap: true,
+            itemCount: keys.length,
+            itemBuilder: (BuildContext context, int index) {
+              final int key = keys[index];
+              final dynamic route = box.get(key);
+              return Text(route.name);
             },
-          ),
-          // ListView.builder(
-          //   shrinkWrap: true,
-          //   itemCount: routes.length,
-          //   itemBuilder: (BuildContext context, int index) {
-          //     // access element from list using index
-          //     // you can create and return a widget of your choice
-          //     return Text(routes[index].name);
-          //   },
-          // ),
-        ],
+          );
+        },
       ),
     );
   }
